a2fd2d6c69582abc48764b27019801c12c3fadaa
DiegoVillacis <95387849+DiegoVillacis@users.noreply.github.com>
2022-07-14 20:00:49 -0500

    Commit Dashboard 7/14/2022 8:00 pm

--- app/src/main/java/fisei/reyespc/shopclothes_app/UI/DashboardActivity.java
+++ app/src/main/java/fisei/reyespc/shopclothes_app/UI/DashboardActivity.java
@@ -10,6 +10,7 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_dashboard);
     }
 }
